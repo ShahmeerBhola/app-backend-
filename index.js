4bff005e7e834a57c47d759e6a16887f3b05fe1b
ShahmeerBhola <shahmir.shamim1@gmail.com>
2023-03-25 04:05:02 +0500

    pushed

--- index.js
+++ index.js
@@ -1,21 +1,61 @@
-const path = require("path");
-const { createServer } = require("http");
+// const path = require("path");
+// const { createServer } = require("http");
+
+// const express = require("express");
+// const { getIO, initIO } = require("./socket");
+
+// const app = express();
+
+// let port = process.env.PORT || 3500;
+// const httpServer = createServer(app);
+
+// initIO(httpServer);
+// // app.use('/', express.static(path.join(__dirname, 'static')));
+
+// app.get("/api", (req, res) => {
+//   res.json("server running successfully.");
+// });
+
+// httpServer.listen(port);
+// console.log("Server started on ", port);
+
+// getIO();
 
 const express = require("express");
-const { getIO, initIO } = require("./socket");
-
 const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
 
-let port = process.env.PORT || 3500;
-const httpServer = createServer(app);
+// app.use(express.static(__dirname + '/public'));
 
-initIO(httpServer);
-// app.use('/', express.static(path.join(__dirname, 'static')));
+io.on("connection", (socket) => {
+  console.log("New client connected");
 
-app.get("/api", (req, res) => {
-  res.json("server running successfully.");
+  socket.on("join", (room) => {
+    console.log("User joined room:", room);
+    socket.join(room);
+  });
+
+  socket.on("offer", (room, offer) => {
+    console.log("Received offer from user in room", room);
+    socket.to(room).emit("offer", offer);
+  });
+
+  socket.on("answer", (room, answer) => {
+    console.log("Received answer from user in room", room);
+    socket.to(room).emit("answer", answer);
+  });
+
+  socket.on("ice", (room, candidate) => {
+    console.log("Received ICE candidate from user in room", room);
+    socket.to(room).emit("ice", candidate);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
 });
 
-httpServer.listen(port);
-console.log("Server started on ", port);
-getIO();
+server.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
